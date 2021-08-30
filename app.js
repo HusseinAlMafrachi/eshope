1c5d8d3923a4f157a95e6710324308e7aa7231c8
Hussein Al Mafrachi <81489485+HusseinAlMafrachi@users.noreply.github.com>
2021-08-30 07:59:11 -0700

    Simple cookie added

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-import NavBar from './components/navbar';
+//import NavBar from './components/navbar';
 //cookies pop up massege
+//This is a simple cookie message and will update to accepting the cookies policy
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 //slide show
-//vido if needed  
+//video if needed  
